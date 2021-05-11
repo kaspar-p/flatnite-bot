7fc246463deb9544aa4680e0f1d1b12ff0e4af75
Kaspar Poland <kaspar78@mouco.com>
2021-05-11 01:17:05 -0600

    Update handlers.js

--- handlers/handlers.js
+++ handlers/handlers.js
@@ -1,5 +1,6 @@
 const _ = require("lodash");
 const fs = require("fs");
+const path = require("path");
 const { commands } = require("../constants/commands");
 const { handleUserInput, refreshSite } = require("../crawl");
 const { CHANNEL, CLASSES } = require("../constants/constants");
@@ -215,14 +216,43 @@ const deregisterHandler = async (client, msg) => {
 };
 
 const howManyHandler = async (client) => {
-  const data = fs.readFileSync("../analysis/data/times.txt", {
+  const linksPath = path.join(process.env.PWD, "/analysis/data/links.txt");
+
+  const linksData = fs.readFileSync(linksPath, {
     encoding: "utf-8",
   });
-  const number = parseInt(data.toString().trim());
+
+  const links = linksData
+    .trim()
+    .split("\n")
+    .map((row) => row.trim());
+
+  const specificNumberLinks = (n) =>
+    links.filter((link) => link.length === n).length;
+  const toPercentage = (n) => ((n / links.length) * 100).toFixed(4);
+
+  const numZero = specificNumberLinks(0);
+  const zeroPercentage = toPercentage(numZero);
+
+  const numOne = specificNumberLinks(1);
+  const onePercentage = toPercentage(numOne);
+
+  const numTwo = specificNumberLinks(2);
+  const twoPercentage = toPercentage(numTwo);
+
+  const numThree = specificNumberLinks(3);
+  const threePercentage = toPercentage(numThree);
+
+  const numFour = specificNumberLinks(4);
+  const fourPercentage = toPercentage(numFour);
 
   sendMessage(
     client,
-    `Gamingbot has done ${number} rounds of statistical analysis`
+    `Links of length 0  :::  ${numZero} / ${links.length} = ${zeroPercentage}\n` +
+      `Links of length 1  :::  ${numOne} / ${links.length} = ${onePercentage}\n` +
+      `Links of length 2  :::  ${numTwo} / ${links.length} = ${twoPercentage}\n` +
+      `Links of length 3  :::  ${numThree} / ${links.length} = ${threePercentage}\n` +
+      `Links of length 4  :::  ${numFour} / ${links.length} = ${fourPercentage}`
   );
 };
 
